df9cff08ca4523cfa5afb7482bc0d52c496a9e5c
devhiteshk <dev.hitesh.k@gmail.com>
2022-12-23 18:17:37 +0530

    "working phone book improved"

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,12 +3,8 @@ import { useAlert } from "react-alert";
 import Filter from "./Components/filter";
 import PersonForm from "./Components/personForm";
 import Persons from "./Components/Persons";
-let db = [
-  { name: "Arto Hellas", number: "040-123456", id: 1 },
-  { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-  { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-  { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-];
+import DB from "./db";
+let db = DB;
 
 const App = () => {
   const alert = useAlert();
---
+++ part2/phonebook/src/db.js
@@ -0,0 +1,40 @@
+let db = [
+  {
+    name: "Aguirre Dunlap",
+    number: "(901) 516-3344",
+  },
+  {
+    name: "Jeanine Donovan",
+    number: "(855) 538-2140",
+  },
+  {
+    name: "Carla Obrien",
+    number: "(892) 531-2584",
+  },
+  {
+    name: "Shari Roth",
+    number: "(975) 457-3455",
+  },
+  {
+    name: "Rene Lowery",
+    number: "(936) 523-3921",
+  },
+  {
+    name: "Marcella Kennedy",
+    number: "(940) 452-2194",
+  },
+  {
+    name: "Wallace Barr",
+    number: "(845) 592-3709",
+  },
+  {
+    name: "Weber Cleveland",
+    number: "(908) 545-2242",
+  },
+  {
+    name: "Mills Wood",
+    number: "(978) 500-2783",
+  },
+];
+
+export default db;
